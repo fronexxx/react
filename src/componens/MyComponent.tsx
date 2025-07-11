281f3f3195ae528a8efd915697d4f968ab4f1cf5
fronexxx <igorbotsii42@gmail.com>
2025-07-11 23:05:08 +0300

    base and module styles

--- src/componens/MyComponent.tsx
+++ src/componens/MyComponent.tsx
@@ -1,9 +1,16 @@
 import type {FC} from "react";
+import styles from './MyComponent.module.css';
+// import './MyComponent.module.css'
+
 
 type MyComponentPropType = { text: string }
 
-const MyComponent: FC<MyComponentPropType> = ({text})=> {
-    return <div>{text}</div>
-}
+const MyComponent: FC<MyComponentPropType> = ({text}) => {
+    return (
+        <div className={styles.target}>
+            {text}
+        </div>
+    );
+};
 export default MyComponent;
 
