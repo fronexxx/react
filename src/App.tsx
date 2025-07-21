21d760e1929e71f04fe9c215ec5ed7ef78974b65
fronexxx <igorbotsii42@gmail.com>
2025-07-21 13:50:08 +0300

    Children

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ function App() {
             <MyComponent title={'Hello 1'}>
                 Lorem ipsum dolor sit.
             </MyComponent>
-            <MyComponent title={'Hello 2'}/>
+            <MyComponent title={'Hello 2'}>
+
+            </MyComponent>
             <MyComponent title={'Hello 3'}/>
-            <MyComponent title={'Hello okten'}/>
             {/*{MyComponent({text: 'Hello 2'})}*/}
         </>
     );
