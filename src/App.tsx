79705ddc6ce30c96e9f2d5dcdc45da78fc3dfb9c
fronexxx <igorbotsii42@gmail.com>
2025-07-21 13:39:52 +0300

    Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
-import MyComponent from "./componens/MyComponent.tsx";
+import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
     return (
         <>
-            <MyComponent text={'Hello 1'}/>
-            <MyComponent text={'Hello 2'}/>
-            <MyComponent text={'Hello 3'}/>
-            <MyComponent text={'Hello okten'}/>
+            <MyComponent title={'Hello 1'}>
+                Lorem ipsum dolor sit.
+            </MyComponent>
+            <MyComponent title={'Hello 2'}/>
+            <MyComponent title={'Hello 3'}/>
+            <MyComponent title={'Hello okten'}/>
             {/*{MyComponent({text: 'Hello 2'})}*/}
         </>
     );
