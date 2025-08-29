1c0a4983d6af93b5d5d65de6194ace19c7637d34
fronexxx <igorbotsii42@gmail.com>
2025-08-29 14:55:12 +0300

    lessons/lesson2/services

--- src/components/users-component/UsersComponent.tsx
+++ src/components/users-component/UsersComponent.tsx
@@ -1,16 +1,18 @@
 import {useEffect, useState} from "react";
 import type {IUser} from "../../models/IUser.ts";
 import UserComponent from "../user-component/UserComponent.tsx";
+import {getUsers} from "../../services/api.service.ts";
 
 export const UsersComponent = () => {
     const [users, setUsers] = useState<IUser[]>([]);
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then((value) => value.json())
-            .then((response) => {
-                setUsers(response);
-            });
+useEffect(() => {
+    const fetchData = async () => {
+        const users = await getUsers();
+        setUsers(users);
+    }
+    fetchData();
+
         return () => {
             console.log('done');
         };
