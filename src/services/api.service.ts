9aa8fa406a9e8f1729808c0920394b479afe432c
fronexxx <igorbotsii42@gmail.com>
2025-08-29 15:20:26 +0300

    lessons/lesson2/env

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,14 +1,15 @@
 import type {IUser} from "../models/IUser.ts";
 
 const getUsers = async (): Promise<IUser[]> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users')
+    return await fetch(import.meta.env.VITE_API_URL)
         .then((value) => value.json());
+
 };
 
 // Для чого окремий файл для фетча? Пояснення нижче:
 
 const getUser = async (id: string): Promise<IUser> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+    return await fetch(import.meta.env.VITE_API_URL + '/' + id)
         .then((value) => value.json());
 };
 
