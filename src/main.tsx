ca5e9689dbe20bbb2767f2cd0c048887d0dfdbf3
fronexxx <igorbotsii42@gmail.com>
2025-09-03 21:16:22 +0300

    lessons/lesson3/Basic-router_v7

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,40 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.tsx";
 import APage from "./pages/APage.tsx";
 import BPage from "./pages/BPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
 
-const router = createBrowserRouter([
-    {
-        path: '/', element: <MainLayout/>,
-        children:
-            [
-                {path: '', element: <HomePage/>},
-                {path: 'a', element: <APage/>},
-                {path: 'b', element: <BPage/>, children: [
-                        {path: '1', element: <div>content 1</div>},
-                        {path: '2', element: <div>content 2</div>},
-                        {path: '3', element: <div>content 3</div>}
-                    ]}
-            ]
-    },
-]);
+// const router = createBrowserRouter([
+//     {
+//         path: '/', element: <MainLayout/>,
+//         children:
+//             [
+//                 {path: '', element: <HomePage/>},
+//                 {path: 'a', element: <APage/>},
+//                 {path: 'b', element: <BPage/>, children: [
+//                         {path: '1', element: <div>content 1</div>},
+//                         {path: '2', element: <div>content 2</div>},
+//                         {path: '3', element: <div>content 3</div>}
+//                     ]}
+//             ]
+//     },
+// ]);
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={router}/>)
+    .render(
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<MainLayout/>}>
+                    <Route path={''} element={<HomePage/>}/>
+                    <Route path={'a'} element={<APage/>}/>
+                    <Route path={'b'} element={<BPage/>}>
+                        <Route path={'1'} element={<div>Content 1</div>}/>
+                        <Route path={'2'} element={<div>Content 2</div>}/>
+                        <Route path={'3'} element={<div>Content 3</div>}/>
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
