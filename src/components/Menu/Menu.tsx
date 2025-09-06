e40e046df7effd52c83cf81abd135f7348ae588b
fronexxx <igorbotsii42@gmail.com>
2025-09-06 12:08:05 +0300

    Homeworks/HW_3/Task-1_Part-2

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,6 +5,7 @@ const Menu = () => {
     return (
         <div>
             <ul>
+                <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'users'}>users</Link></li>
                 <li><Link to={'posts'}>posts</Link></li>
                 <li><Link to={'comments'}>comments</Link></li>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,22 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import {router} from "./router/Routes.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import UsersComponent from "./components/users/UsersComponent.tsx";
+import PostsComponent from "./components/posts/PostsComponent.tsx";
+import ProductsComponent from "./components/products/ProductsComponent.tsx";
+import App from "./App.tsx";
+import CommentsComponent from "./components/comments/CommentsComponent.tsx";
 
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={router}/>)
+    .render(<BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<App/>}>
+                <Route path={''} element={<div>this is Home Page</div>}/>
+                <Route path={'users'} element={<UsersComponent/>}/>
+                <Route path={'posts'} element={<PostsComponent/>}/>
+                <Route path={'comments'} element={<CommentsComponent/>}/>
+                <Route path={'products'} element={<ProductsComponent/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>)
