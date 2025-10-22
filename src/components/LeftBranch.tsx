01ba6d6875e7c569f3bdc0df002c91992449c969
fronexxx <igorbotsii42@gmail.com>
2025-10-22 13:23:54 +0300

    lessons/lesson7/useMemo+context_demo

--- src/components/LeftBranch.tsx
+++ src/components/LeftBranch.tsx
@@ -1,6 +1,13 @@
 import LeftBranchA from "./LeftBranchA.tsx";
+import {useMemo} from "react";
 
 const LeftBranch = () => {
+    const memox = useMemo(() => {
+        for (let i = 0; i < 1000; i++) {
+                console.log(i);
+        }
+        return null;
+    }, []);
     return (
         <div>Left Branch
             <LeftBranchA/>
