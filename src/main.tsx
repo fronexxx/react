3fff6789540680151346d059fc781f27071fa3ea
fronexxx <igorbotsii42@gmail.com>
2025-10-29 13:44:23 +0200

    lessons/lesson8/redux-2

--- src/main.tsx
+++ src/main.tsx
@@ -2,38 +2,9 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import {router} from "./router/router.tsx";
-import {Provider, useSelector} from "react-redux";
-import {configureStore, createSlice, type PayloadAction} from "@reduxjs/toolkit";
-import type {IUser} from "./models/IUser.ts";
+import {Provider} from "react-redux";
+import {store} from "./redux/store.ts";
 
-type UsersSliceType = {
-    users: IUser[]
-}
-
-const initialState: UsersSliceType = {users:[]};
-export const userSlice = createSlice({
-    name: "userSlice",
-    initialState: initialState,
-    reducers: {
-        loadUsers: (state, action: PayloadAction<IUser[]>) => {
-            state.users = action.payload
-        },
-
-    }
-});
-
-export const userSliceActions = {
-    ...userSlice.actions,
-}
-
-const store = configureStore({
-    reducer: {
-        userSlice: userSlice.reducer,
-        // postSlice: null
-    },
-});
-
-export const useAppSelector = useSelector.withTypes<ReturnType<typeof store.getState>>();
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
